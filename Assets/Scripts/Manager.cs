74b9af63bd3444e5a20087a37e6f945f5f00d177
G. Y <goktug7913@gmail.com>
2023-10-12 02:19:29 +0300

    add: core functions

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,30 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace DefaultNamespace
+public class Manager : MonoBehaviour
 {
-    public class Manager : MonoBehaviour
-    {
-        public List<Agent> agents = new();
+    public static Manager instance;
+    
+    public List<Agent> agents = new();
 
-        private void Start()
+    private static float boundsSize = 20f;
+    public Rect bounds = new(-boundsSize, -boundsSize, boundsSize * 2, boundsSize * 2);
+    
+    private void Start()
+    {
+        if (instance == null)
         {
-            throw new NotImplementedException();
+            instance = this;
+        }
+        else
+        {
+            throw new UnityException("Multiple managers in scene");
         }
         
-        private void Update()
+        // Spawn 10 prey
+        for (var i = 0; i < 10; i++)
         {
-            throw new NotImplementedException();
+            SpawnAgent(AgentType.Prey);
         }
         
-        public Agent SpawnAgent(AgentType type, Vector2 position)
-        {
-            throw new NotImplementedException();
-        }
+        // Spawn 1 predator
+        SpawnAgent(AgentType.Predator);
+    }
         
-        public Agent SpawnAgent(AgentType type)
+    private void Update()
+    {
+        // Spawn a new agent on spacebar press
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            throw new NotImplementedException();
-        }                                  
+            SpawnAgent(AgentType.Predator);
+        }
+    }
+        
+    public Agent SpawnAgent(AgentType type, Vector2 position)
+    {
+        var obj = new GameObject("Agent", typeof(Agent));
+        
+        var agent = obj.GetComponent<Agent>();
+        
+        agent.type = type;
+        agent.transform.position = position;
+        
+        agents.Add(agent);
+        
+        return agent;
+    }
+        
+    public Agent SpawnAgent(AgentType type)
+    {
+        return SpawnAgent(
+            type,
+            GetRandPosInBounds()
+        );
+    }             
+    
+    private Vector2 GetRandPosInBounds()
+    {
+        return new Vector2(
+            UnityEngine.Random.Range(bounds.xMin, bounds.xMax),
+            UnityEngine.Random.Range(bounds.yMin, bounds.yMax)
+        );
     }
 }
